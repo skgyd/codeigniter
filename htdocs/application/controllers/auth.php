cf3826dc8c7bd19187dc4c9c448e4a8dcd6640e3
unknown <nhpark326@naver.com>
2021-05-26 15:27:17 +0900

    join 추가

--- htdocs/application/controllers/auth.php
+++ htdocs/application/controllers/auth.php
@@ -6,7 +6,7 @@ class Auth extends MY_Controller{
 
     function login(){
         $this->load->config('opentutorials');
-        $this->load->view('head');     
+        $this->_head();     
         $this->load->view('login');
         $this->_footer();
     }
@@ -15,6 +15,22 @@ class Auth extends MY_Controller{
         $this->session->sess_destroy();
         $this->load->helper('url');
         redirect("topic");
+    }
+
+    function join(){
+        $this->_head();
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('email','이메일주소','required|valid_email|is_unique[user.email]');
+        $this->form_validation->set_rules('nickname','닉네임','required|min_length[5]|max_length[20]');
+        $this->form_validation->set_rules('password','비밀번호','required|min_length[6]|max_length[30]|matches[re_password]');
+        $this->form_validation->set_rules('re_password','비밀번호 확인','required');
+        if($this->form_validation->run() === false){
+            $this->load->view('join');
+        } else{
+            //descryption
+            //add DB
+        }
+        $this->_footer();
     }
 
     function authentication(){
--- htdocs/application/views/head.php
+++ htdocs/application/views/head.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf=8"/>
         <!-- static/css연동 -->
-        <link href="/static/css/style.css?refresh" rel="stylesheet" type="text/css" media="all"/>
+        <link href="/static/css/style.css?" rel="stylesheet" type="text/css" media="all"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- <style>
             body{
@@ -32,6 +32,19 @@
                         <span class="icon-bar"></span>
                     </a>
                     <a class="brand" href="/index.php/topic" ><h2>Topic</h2></a>
+                    <?php
+                        if($this->config->item('is_dev')){
+                        ?>
+                            <div class="well span12" style="margin:15px 10px; color:red; background-color:green;">
+                                <b>개발환경 수정 중입니다.</b><br>
+                            </div>
+                            <!-- <?php
+                            var_dump($this->config->item('is_dev'));
+                            ?> -->
+                        <?php
+                        }
+                    ?>
+            
                     <div calss="nav-collapse collapse">
                         <div class="nav pull-right">
                             <?php
@@ -51,17 +64,5 @@
                 </div>
             </div>
         </div>
-        <?php
-            if($this->config->item('is_dev')){
-            ?>
-                <div class="well span12" style="margin:10px; color:red">
-                    <b>개발환경 수정 중입니다.</b><br>
-                </div>
-                <!-- <?php
-                var_dump($this->config->item('is_dev'));
-                ?> -->
-            <?php
-            }
-        ?>
-            
+        
         
